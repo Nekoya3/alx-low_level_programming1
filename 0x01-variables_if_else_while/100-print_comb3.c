1c8610b8251747995d348d5a10e7caa636b8b68f
Nekoya3 <anekoye3@gmail.com>
2022-09-11 02:21:51 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,9 +5,8 @@
  * Numbers must be separated by ,, followed by a space
  * The two digits must be different
  * 01 and 10 are considered the same combination of the two digits 0 and 1
- * Print only the smallest combination of two digits
- * Numbers should be printed in ascending order, with two digits
- * Return: 0
+ *
+ *Return: 0
  */
 
 int main(void)
