72a6caae8ec0baceac64412b4259b6a0f4746862
Nekoya3 <anekoye3@gmail.com>
2022-09-11 03:42:49 -0700

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- *  main - program that prints the alphabet in lowercase, and then in uppercase,
+ *  main -program that prints the alphabet in lowercase, and then in uppercase
  *  You can only use the putchar
  *  Return: 0
  */
