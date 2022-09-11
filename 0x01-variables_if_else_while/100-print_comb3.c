f56be39c39e11b093de331b6eba74be20376d45a
Nekoya3 <anekoye3@gmail.com>
2022-09-11 02:42:52 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,7 +3,7 @@
  * main - program that prints all possible different combinations of two digits
  * Numbers must be separated by ,, followed by a space
  * The two digits must be different
- * 
+ *
  * Return: Always (Success)
  */
 int main(void)
