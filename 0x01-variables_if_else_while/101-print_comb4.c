50a6fa2a8a1ed167a1b0e800aac49fb6c3d3a629
Nekoya3 <anekoye3@gmail.com>
2022-09-11 02:56:31 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -4,7 +4,6 @@
  * main - program that prints all possible different combinations of three digits
  * Numbers must be separated by ,, followed by a space
  * The three digits must be different
- * 012, 120, 102, 021, 201, 210 are considered the same combination of the three digits 0, 1 and 2
  *
  * Return: Always (Success)
  */
