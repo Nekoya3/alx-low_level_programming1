77eaafa9ec0a3b7ef04e979c72b5b8aeaa16c84f
Nekoya3 <anekoye3@gmail.com>
2022-09-13 02:19:42 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,19 +3,20 @@
 /**
  * main - Entry point
  *
- * Return: Always 0
+ * Return: nothing, return void
  */
 int main(void)
 {
-       	_putchar('_'); 
-	_putchar('p'); 
-	_putchar('u'); 
-	_putchar('t'); 
-	_putchar('c'); 
-	_putchar('h'); 
-	_putchar('a'); 
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
 	_putchar('r');
-	_putchar('\n'); 
+	_putchar('\n');
 	
 	return (0);
 }
+
