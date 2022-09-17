70f79a3c227a2debf01bd0cf8f2ba0a4594af51f
Nekoya3 <anekoye3@gmail.com>
2022-09-17 05:26:17 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,42 +1,39 @@
 #include "main.h"
 
 /**
- * times_table - prints the 9 times table
- * Description: prints the 9 times table
+ * times_table -  a function that prints the 9 times table, starting with 0
+ * rone = row, cone = column, d = digits of current result
  * Return: times table
  * add extra space past single digit
  */
-
 void times_table(void)
 {
-	int row, column, product, tens, ones;
+	int rone, cone, d;
 
-	for (row = 0; row <= 9; row++)
+	for (rone = 0; rone <= 9; rone++)
 	{
-		for (column = 0; column <= 9; column++)
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (cone = 1; cone <= 9; cone++)
 		{
-			product = row * column;
-			tens = product / 10;
-			ones = product % 10;
-
-			if (column == 0)
+			d = (rone * cone);
+			if ((d / 10) > 0)
 			{
-				_putchar('0');
-			}
-			else if (product < 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(ones + '0');
+				_putchar((d / 10) + '0');
 			}
 			else
 			{
+				_putchar(' ');
+			}
+			_putchar((d % 10) + '0');
+
+			if (cone < 9)
+			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(tens + '0');
-				_putchar(ones + '0');
 			}
 		}
+		_putchar('\n');
 	}
 }
