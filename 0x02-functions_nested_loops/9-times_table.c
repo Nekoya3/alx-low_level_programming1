d55ad40e46336126622c8384cafb6750f7af57a3
Nekoya3 <anekoye3@gmail.com>
2022-09-17 05:20:18 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,7 +3,8 @@
 /**
  * times_table - prints the 9 times table
  * Description: prints the 9 times table
- * Return: void
+ * Return: times table
+ * add extra space past single digit
  */
 
 void times_table(void)
