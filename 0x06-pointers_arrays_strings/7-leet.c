8d13e128a7a1e3f2d68ef86d8b545da1990fdb31
Nekoya3 <anekoye3@gmail.com>
2022-09-21 10:31:26 -0700

    Tas\k 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,23 +1,25 @@
 #include "main.h"
 /**
- * leet - function that encode a string
- * @str:string that will be encoded
- * Return:returns encoded string
+ * leet - replaces a bunch of numbers with letters
+ *
+ * @z: the string to look at
+ *
+ * Return: the new string
  */
 
-char *leet(char *str)
+char *leet(char *z)
 {
-	int index1 = 0, index2;
-	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+	char arr[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char replace[] = {'4', '3', '0', '7', '1'};
+	int i, j;
 
-	while (str[++index1])
+	for (i = 0; z[i] != '\0'; i++)
 	{
-		for (index2 = 0; index2 <= 7; index2++)
+		for (j = 0; arr[j] != '\0'; j++)
 		{
-			if (str[index1] == leet[index2] ||
-					str[index1] - 32 == leet[index2])
-				str[index1] = index2 + '0';
+			if (z[i] == arr[j])
+				z[i] = replace[j / 2];
 		}
 	}
-	return (str);
+	return (z);
 }
