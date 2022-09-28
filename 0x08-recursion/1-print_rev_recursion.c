9affb2c661c559ebd0230ef2919fe171116abd2f
Nekoya3 <anekoye3@gmail.com>
2022-09-28 02:33:56 -0700

    Task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a tring in reverse
+ * @s: string to print
+ *
+ * return - does not retrun anything
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
