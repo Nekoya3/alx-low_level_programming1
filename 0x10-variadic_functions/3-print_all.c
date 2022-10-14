9d819c0235c7788d337994203ec389cd4b170052
Nekoya3 <anekoye3@gmail.com>
2022-10-14 15:19:18 +0300

    Task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,52 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints anything
+ * @format: type arguments
+ * Return: nothing
+ */
+void print_all(const char * const format, ...)
+{
+	int x = 0;
+	va_list args;
+	char *st;
+
+	while (format == NULL)
+	{
+		printf("\n");
+		return;
+	}
+	va_start(args, format);
+	while (format[x])
+	{
+		switch (format[x])
+		{
+		case 'c':
+			printf("%c", va_arg(args, int));
+			break;
+		case 'i':
+			printf("%i", va_arg(args, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(args, double));
+			break;
+		case 's':
+			st = va_arg(args, char*);
+
+			if (st == NULL)
+			{
+				printf("(nil)");
+				break;
+			}
+			printf("%s", st);
+			break;
+		default:
+			x++;
+			continue;
+		}
+		if (format[(x + 1)] != '\0')
+			printf(", ");
+		x++;
+	}
+	printf("\n");
+
